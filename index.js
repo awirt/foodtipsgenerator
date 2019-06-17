602152a71c91cabb11942776642e5b7fa8074fc6
Andrew Wirt <15827151+awirt@users.noreply.github.com>
2019-06-17 00:42:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var subtitle = $('.subtitle');
 console.log(subtitle);
 
 
-const pasta = ["Don't rinse your pasta, or you'll get rid of all the starches that help the sauce cling to it.", 
+var pasta = ["Don't rinse your pasta, or you'll get rid of all the starches that help the sauce cling to it.", 
             "Always finish your pasta in the sauce!",
             "Slightly undercook your pasta, so that when you finish it off in the sauce, it will cook to your liking",
             "For thick, chunky sauces, pick a pasta with wide grooves"];
